c42425e5f33e1357f932ce3648a91fd53c11247a
radek.jancik@sunamo.cz <radek.jancik@sunamo.cz>
2023-09-28 14:46:48 +0200

    Knowledge is power.

--- GridControlsInWpf_Blog/Source.cs
+++ GridControlsInWpf_Blog/Source.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 public class Source
 {
 static Type type = typeof(Source);
